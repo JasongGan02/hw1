3696bcf5aae1ca42dad997eb09a1faa310292306
JasonGan <ganjason@usc.edu>
2024-01-15 22:03:40 -0800

    Part 1 done

--- split.cpp
+++ split.cpp
@@ -11,13 +11,52 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <cstddef>
 /* Add a prototype for a helper function here if you need */
-
-void split(Node*& in, Node*& odds, Node*& evens)
+/* If you needed a helper function, write it here */
+void splitHelper(Node*& in, Node*& odds, Node*& evens, Node*& lastOdd, Node*& lastEven)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+    if (in == nullptr)
+    {
+        return;
+    }
+
+    Node* nextNode = in->next; // Store the next node
+    in->next = nullptr;
+    // Recursive case: If input list is not empty, split the list
+    if (in->value % 2 == 0) // case the input is even
+    {
+        if (evens == nullptr) //initialize evens if its the first node
+        {
+            evens = in;
+        }
+        else
+        {
+            lastEven->next = in;
+        }
+        lastEven = in;
+    }
+    else //case the input is odd
+    {
+        if (odds == nullptr) //initialize odds if its the first node
+        {
+            odds = in;
+        }
+        else
+        {
+            lastOdd->next = in;
+        }
+        lastOdd = in;
+    }
+    in = nextNode;
+    splitHelper(nextNode, odds, evens, lastOdd, lastEven);
+} 
+
+void split(Node*& in, Node*& odds, Node*& evens) {  
+    if (in == nullptr)
+        return;
+    Node* lastOdd = nullptr;
+    Node* lastEven = nullptr;
+    splitHelper(in, odds, evens, lastOdd, lastEven);
 }
 
-/* If you needed a helper function, write it here */
